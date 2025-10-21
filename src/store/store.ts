44b04238315b1f7fc623ed471047801ad69b17ee
mm <mmislam272@gmail.com>
2025-10-22 01:01:29 +0600

    fix

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./features/authSlice.ts";
+import authReducer from "./features/authSlice";
 
 export const store = configureStore({
   reducer: {
