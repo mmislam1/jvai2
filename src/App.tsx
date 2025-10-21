0a93adcde698e1b2e8371886059ed30545a5821c
mm <mmislam272@gmail.com>
2025-10-22 03:00:04 +0600

    signin layout basic

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App bg-black">
+      <header >  
+        <div className="background flex flex-col items-center justify-center"
+          style={{ backgroundImage: "url('/children.jpg')" }}>
+          <div className="background flex flex-col items-center justify-center bg-black/50">
+
+            <div className="box1 flex flex-col items-center justify-center bg-[rgb(231,237,245)] rounded-md w-full lg:w-[40vw]  p-4">
+              <h1 className="heading">Log In</h1>
+            </div>
+          </div>
+        </div>   
       </header>
     </div>
   );
--- src/components/signinLayout.tsx
+++ src/components/signinLayout.tsx
@@ -6,19 +6,13 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
     return (
-        <div className="min-h-screen flex flex-col">
-            {/* Header */}
-            <header className="bg-gray-800 text-white p-4">
-                <h1 className="text-xl font-bold">My App</h1>
-            </header>
+        <div className="background flex flex-col items-center justify-center "
+            style={{ backgroundImage: "url('/children.jpg')" }}>
+            
+            <div className="box1 flex flex-col items-center justify-center bg-[rgba(231, 237, 245, 1)] rounded-mg">
+                
+            </div>
 
-            {/* Main Content */}
-            <main className="flex-1 p-4 bg-gray-100">{children}</main>
-
-            {/* Footer */}
-            <footer className="bg-gray-800 text-white p-4 text-center">
-                &copy; {new Date().getFullYear()} My App
-            </footer>
         </div>
     );
 }
