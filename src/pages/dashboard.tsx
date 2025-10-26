186f7e9b3c767825dd2b63922c06b8ac4d96b8ef
mm <mmislam272@gmail.com>
2025-10-26 06:33:09 +0600

    barchart running\

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,29 +1,50 @@
-import React from 'react'
-import Sidebar from '../components/sidebar'
-import UserBar from '../components/userBar'
-import InfoTabs from '../components/infoTabs'
-import { RootState } from '../store/store';
-import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
-import BarChart from '../components/BarChart';
+import React from "react";
+import Sidebar from "../components/sidebar";
+import UserBar from "../components/userBar";
+import InfoTabs from "../components/infoTabs";
+import { RootState } from "../store/store";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import BarChart from "../components/BarChart";
 
 const Dashboard = () => {
-  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.auth.isAuthenticated
+  );
 
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
   }
 
   return (
-    
-      <Sidebar>
-        <InfoTabs/>
-        <div className="flex flex-col items-center justify-center ">
-        <BarChart data={[{ label: 'may', value: 99 },{ label: 'may', value: 79 },{ label: 'may', value: 29 }]} title={'Test'}></BarChart>
+    <Sidebar>
+      <InfoTabs />
+      <div className="flex flex-col items-center justify-center h-full w-full">
+        <div className="flex flex-row items-center h-full justify-between w-full gap-6">
+          <div className="flex-[7] bg-white flex flex-col items-center h-full justify-center rounded-xl p-6">
+            <BarChart
+              data={[
+                { label: "may", value: 99 },
+                { label: "may", value: 79 },
+                { label: "may", value: 29 },
+                { label: "may", value: 99 },
+                { label: "may", value: 79 },
+                { label: "may", value: 29 },
+                { label: "may", value: 99 },
+                { label: "may", value: 79 },
+                { label: "may", value: 29 },
+                { label: "may", value: 99 },
+                { label: "may", value: 79 },
+                { label: "may", value: 29 },
+              ]}
+              title={"Test"}
+            ></BarChart>
+          </div>
+          <div className="flex-[4] bg-white flex flex-col items-center h-full justify-center rounded-xl p-6"></div>
         </div>
-      </Sidebar>
-    
-  )
-}
+      </div>
+    </Sidebar>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
