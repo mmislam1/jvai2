8e9f94d0c596daeaef667ad5aa07d2f796fae641
mm <mmislam272@gmail.com>
2025-10-28 00:44:20 +0600

    infotab fixed

--- src/components/infoTabs.tsx
+++ src/components/infoTabs.tsx
@@ -1,30 +1,38 @@
 import React from 'react'
+import Icon from './icon'
+import { FaCrown } from 'react-icons/fa'
+import { IconType } from 'react-icons'
 
 interface data {
-    title: string | '',
-    info: string | '',
-    color: string | ''
+    title?: string | '',
+    info?: string | '',
+    icon?: IconType ,
+    color?: string | ''
 }
 
 const data: data[] = [
     {
         title: 'Monthly Revenue',
         info: '$12,101',
+        icon: FaCrown,
         color: 'rgba(19,76,156,1)'
     },
     {
         title: 'Monthly Revenue',
         info: '$12,101',
+        icon: FaCrown,
         color: 'rgba(151, 19, 156, 1)'
     },
     {
         title: 'Monthly Revenue',
         info: '$12,101',
+        icon: FaCrown,
         color: 'rgba(19, 156, 99, 1)'
     },
     {
         title: 'Monthly Revenue',
         info: '$12,101',
+        icon: FaCrown,
         color: 'rgba(156, 19, 19, 1)'
     },
 
@@ -38,7 +46,7 @@ const InfoTabs = () => {
                     <div className="flex flex-row p-2 rounded-xl bg-white items-center justify-center h-[144px] w-full md:w-[368px] ">
                         <div className="flex flex-[2] h-full bg-white items-center justify-center ">
                             <div className={`flex flex-row items-center justify-center rounded-xl h-[100px] w-[100px]`} style={{ backgroundColor: item.color, opacity:.2}}>
-
+                                <Icon icon={FaCrown}></Icon>
                             </div>
                         </div>
                         <div className="flex flex-col flex-[3] h-full bg-white items-left justify-center ">
